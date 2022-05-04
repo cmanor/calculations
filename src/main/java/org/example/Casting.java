e66e98fb5b36470db229648934b4beab63dbdbc6
cmanor <101116379+cmanor@users.noreply.github.com>
2022-05-04 11:03:17 -0400

    casting complete

--- src/main/java/org/example/Casting.java
+++ src/main/java/org/example/Casting.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Casting {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double var1 =112.35;
+        int var2 = (int)var1;
+        System.out.println("Double: " + var1);
+        System.out.println("int: " + var2 );
+        String str1 = "49";
+        int str2 = Integer.parseInt(str1);
+        System.out.println("The string value is: " + str1);
+        System.out.println("The integer value is: " + str2);
     }
 }
