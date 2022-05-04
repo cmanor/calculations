f6e9f55f749a904c8bc2b2db9ffc8b0daccb1717
cmanor <101116379+cmanor@users.noreply.github.com>
2022-05-04 10:48:08 -0400

    calculations complete

---
+++ src/main/java/org/example/Calculations.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Calculations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter another number:");
+        int num2 = scanner.nextInt();
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+        System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+        System.out.println(num1 + " / " + num2 + " = " + ((double)num1 / (double)num2));
+        System.out.println(num1 + " % " + num2 + " = " + (num1 % num2));
+    }
+}
